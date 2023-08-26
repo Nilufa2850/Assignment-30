78632115d7114a02a1daac1a0f51afd4ba20e001
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2023-08-26 23:43:52 +0530

    Create Qn-04.cpp

---
+++ Qn-04.cpp
@@ -0,0 +1,37 @@
+/*4. Write a C++ program to accept an email address and throw an exception if it does not
+contain @ symbol.*/
+
+#include<iostream>
+#include<cstring>
+using namespace std ;
+int main ()
+{
+    char email[30] ;
+    
+    
+    try 
+    {
+        cout<<"Enter an email address : " ;
+        cin.getline(email , 30) ;
+
+        int i , count=0 ;
+        for(i=0 ; email[i] ; i++)
+        {
+            if (email[i] == '@')
+                count ++ ;
+        }
+        if (count == 0)
+            throw 1 ;
+
+        cout<<"EmailId is "<<email <<endl ;
+    }
+    catch (int m)
+    {
+        cout<<"@ not present" ;
+    }
+    catch(...)
+    {
+        cout<<"Invalid email" ;
+    }
+    return 0 ;
+}
